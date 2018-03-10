764aa1e1ad6b6ff61c2463d7d1673a712ddbd10c
Alvin Chan <aschan@gmail.com>
2018-03-09 16:34:06 -0800

    Update route action types to delimit with slashes

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
 // Action types
-export const HOME = 'ROUTES.HOME'
-export const FORM = 'ROUTES.FORM'
+export const HOME = 'ROUTES/HOME'
+export const FORM = 'ROUTES/FORM'
 
 // Routes
 export default {
